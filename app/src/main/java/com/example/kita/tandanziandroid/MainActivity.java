4ecfd97ab2826c4a2b27b8b3d735626d1ef0de53
charlotte4157@gmail.com <charlotte4157@gmail.com>
2018-01-03 13:46:18 +0900

    test1

--- app/src/main/java/com/example/kita/tandanziandroid/MainActivity.java
+++ app/src/main/java/com/example/kita/tandanziandroid/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.kita.tandanziandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.example.kita.tandanziandroid.R;
 
@@ -11,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Log.v("momo","momo");
     }
 }
