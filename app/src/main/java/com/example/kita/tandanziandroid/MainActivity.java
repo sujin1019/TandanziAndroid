f4d2f75c4502c33e53bded8241d449efb1431325
sujin <sujin.susan.lee@gmail.com>
2018-01-03 17:13:02 +0900

    회원가입 ui 생성

--- app/src/main/java/com/example/kita/tandanziandroid/MainActivity.java
+++ app/src/main/java/com/example/kita/tandanziandroid/MainActivity.java
@@ -3,15 +3,26 @@ package com.example.kita.tandanziandroid;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 import com.example.kita.tandanziandroid.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView tv1, tv2, tv3;
+    String jsonText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.v("momo","momo");
+
+        tv1 = (TextView) findViewById(R.id.tv1);
+        tv2 = (TextView) findViewById(R.id.tv2);
+        tv3 = (TextView) findViewById(R.id.tv3);
+    }
+
+    public void join(){
+
     }
 }
