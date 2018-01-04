9693acf27c2edf4c2264dba248871133570cdd8d
sujin <sujin.susan.lee@gmail.com>
2018-01-04 17:08:38 +0900

    회원가입 main activity 수정

--- app/src/main/java/com/example/kita/tandanziandroid/MainActivity.java
+++ app/src/main/java/com/example/kita/tandanziandroid/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.kita.tandanziandroid;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -8,13 +10,19 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText et1, et2, et3;
-    String userid, pw, weight;
+    String userid, pw, weight, jsonText;
+    HttpURLConnection conn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,47 +34,87 @@ public class MainActivity extends AppCompatActivity {
         et3 = findViewById(R.id.weight);
     }
 
-    public void join(View view){
-    //    String addr = "http://203.233.199.77:8889/tandanzi/join";
+    public void join(View view) {
+        //    String addr = "http://203.233.199.77:8889/tandanzi/join";
         int id = view.getId();
 
         userid = et1.getText().toString().trim();
         pw = et2.getText().toString().trim();
         weight = et3.getText().toString().trim();
 
-        if(userid.length()==0 || pw.length()==0 || weight.length()==0){
+        if (userid.length() == 0 || pw.length() == 0 || weight.length() == 0) {
             Toast.makeText(this, "값을 입력하세요", Toast.LENGTH_SHORT).show();
             return;
         }
 
-        switch (id){
+        switch (id) {
             case R.id.btnJoin:
                 JoinThread joinThread = new JoinThread();
                 joinThread.start();
+
                 break;
         }
 
     }
 
-    class JoinThread extends Thread{
-
+    class JoinThread extends Thread {
         String addr = "http://10.10.8.22:9010/tandanzi/join";
 
         @Override
         public void run() {
             super.run();
+            String str = "";
+            str = setResult();
+            Message msg = handler.obtainMessage();
+            msg.obj = str; // 메시지에 서버에서 가져온 정보 담기
+            handler.sendMessage(msg);
+        }// run()
+
+
+
+        public String setResult() {
+
+            StringBuilder sb = new StringBuilder();
+
             try {
-                URL url = new URL(addr+"?id="+userid+"&pw="+pw+"&weight="+weight);
-                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-                if(conn != null){
+                URL url = new URL(addr + "?id=" + userid + "&pw=" + pw + "&weight=" + weight);
+                conn = (HttpURLConnection) url.openConnection();
+                if (conn != null) {
                     conn.setUseCaches(false); // 캐시메모리를 쓰지 않겠다.
-                    if(conn.getResponseCode()==HttpURLConnection.HTTP_OK){
-                        Log.v("접속", "접속 성공");
+                    if (conn.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                        InputStreamReader in = new InputStreamReader(conn.getInputStream());
+                        int ch;
+                        while ((ch = in.read()) != -1) {
+                            sb.append((char) ch);
+                        }
+                        in.close();
                     }
                 }
-            }catch (Exception e){}
-        }
+                conn.disconnect();
+            } catch (Exception e) {
+            }
 
+            return sb.toString();
+        }
     }
 
+    Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg); // 이 안에 있는 데이터를 TextView 에 뿌려준다.
+         //   Toast.makeText(MainActivity.this, msg.obj.toString(), Toast.LENGTH_SHORT).show();
+            if (msg.obj.toString().equals("success")) {
+                Toast.makeText(MainActivity.this, "가입성공", Toast.LENGTH_SHORT).show();
+            } else if (msg.obj.toString().equals("failed")) {
+                Toast.makeText(MainActivity.this, "다른 아이디를 사용해주세요", Toast.LENGTH_SHORT).show();
+            }
+
+
+        }
+    };
+
+
 }
+
+
+
